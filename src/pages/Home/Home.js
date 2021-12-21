b482bf44787ead80ed7b51a5386a9e8f2302f4dd
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2021-12-21 00:31:40 -0300

    sim

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-
+import Carousel from 'react-bootstrap/Carousel';
 import Navbar from '../../components/Navbar/Navbar';
 import Frame from '../../components/Frame/Frame';
 import Card from '../../components/Card/Card';
@@ -31,11 +31,54 @@ export default function Home() {
   return(
     <div className='home'>
       <Navbar />
-      <Frame 
-        image="https://br.web.img2.acsta.net/pictures/19/02/04/18/35/1468867.jpg"
+    
         
-        
-      />
+      
+        <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block rounded-thumbnail"
+            src="https://br.web.img2.acsta.net/pictures/19/02/04/18/35/1468867.jpg"
+            width="100%"
+            height="350px"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h2><justify> CAPITÃ  MARVEL </justify></h2>
+            <p>Data de estreia dia 25 de dezembro 2021.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block rounded-thumbnail"
+            src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQVxLXB4UpngxtMPNndsVI451JZ6TQSn3XejvWkUX3Bh80WKFlT"
+            width="100%"
+            height="350px"
+            alt="Second slide"
+          />
+      
+          <Carousel.Caption>
+          <h2><justify> VENON</justify></h2>
+            <p>Data de estreia dia 05 de maio de 2022</p>
+             </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block rounded-thumbnail"
+            src="https://portalpopline.com.br/wp-content/uploads/2021/11/homem-aranha-trailer-640x800.jpg" 
+            width="100%"
+            height="350px"
+            alt="Third slide"
+          />
+      
+          <Carousel.Caption>
+          <h2><justify>HOMEM ARANHA 2</justify></h2>
+            <p>Data de estreia dia 05 de maio de 2022</p>
+            </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+   
+
       <div className='home__cards'>
         {
           plantas.map(item => (
